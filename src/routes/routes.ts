68ed0884a9cc163a9d5b5de49034d23fee975c43
FabioRocha231 <fabioharoldo1@gmail.com>
2022-06-15 13:47:46 -0300

    feat: card expiration validator implemented

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,14 +1,16 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import { ValidateCardNumber } from "../validators/CardValidator";
 import { TransactionValidator } from "../validators/cardOwnerValidator";
 import { TransactionValueValidator } from "../validators/transactionValueValidator";
 import { PayMethodValidator } from "../validators/payMethodValidator";
 import { CvvValidator } from "../validators/cvvValidator";
+import { CardValidityValidator } from "../validators/cardValidityValidator";
 
 const routes = Router();
 routes.post(
   "/transaction",
   new ValidateCardNumber().validate,
+  new CardValidityValidator().isValid,
   new TransactionValueValidator().isValid,
   new PayMethodValidator().isValid,
   new CvvValidator().isValid,
---
+++ src/validators/cardValidityValidator.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import { Validator } from "../protocols/validators/transactionValidator";
+
+export class CardValidityValidator implements Validator {
+  async isValid(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Error | Response | void> {
+    let today;
+    const { cardValidity } = req.body;
+    if (!cardValidity)
+      return res.status(406).send({ message: "expiration date is required" });
+    today = new Date();
+    const date = [...cardValidity.split("/")];
+    const month = +date[0]
+    const year = +date[1]
+    const expiryDate = new Date(year, month)
+
+    // console.log(someday)
+    if (expiryDate < today)
+      return res.status(406).send({
+        message:
+          "The expiry date is before today's date. Please select a valid expiry date",
+      });
+
+    next();
+  }
+}
